d442b0c1b8e53c362b02c14a48d203e58117668c
Muhammed Rashid N <muhammed916rashid@gmail.com>
2022-12-28 10:13:17 +0530

    SOmthing is added

--- src/pages/Home/components/Screen1.jsx
+++ src/pages/Home/components/Screen1.jsx
@@ -4,8 +4,8 @@ function Screen1() {
     const [count, setCount] = useState(1)
     return (
         <div className="screen1 h-[600px] px-[10%] flex justify-center  flex-col gap-10 lg:border-b relative">
-            <h1 data-aos="fade-up" className='text-4xl lg:text-6xl font-bold'>Get your <br /> Vape Monkey NFT </h1>
-            <p className='w-full lg:w-1/2 text-xl font-medium'>Once upon a time in a land far far away, there was a special kind of Ape that originated somewhere in Eastern Europe. It has conquered our world and is now on a new journey to take over the metaverse.</p>
+            <h1 data-aos="fade-up" className='text-4xl lg:text-6xl font-bold z-10'>Get your <br /> Vape Monkey NFT </h1>
+            <p className='w-full lg:w-1/2 text-xl font-medium z-10'>Once upon a time in a land far far away, there was a special kind of Ape that originated somewhere in Eastern Europe. It has conquered our world and is now on a new journey to take over the metaverse.</p>
             <div className="absolute bottom-[-190px] inset-x-0 mx-auto bg-black lg:w-[650px] px-[50px] flex flex-col justify-center items-center gap-5">
                 <div className="w-[300px] lg:w-[400px] h-[150px] rounded-[140px] border-2 border-white flex-shrink-0 flex justify-center items-center gap-5">
                     <span className='text-6xl font-bold'>{count}</span>
@@ -18,7 +18,9 @@ function Screen1() {
                     Connect
                 </button>
             </div>
-            
+            <div className="hidden md:block absolute -top-36 rotate-[18deg] -right-28 h-full  z-0">
+                <img src="/image/home/banner.png" className='h-full object-cover' alt="" />
+            </div>
         </div>
     )
 }
