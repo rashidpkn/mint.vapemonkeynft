b4635abf12b08b10fac6ab91a2cdbd19de557a82
Muhammed Rashid N <muhammed916rashid@gmail.com>
2023-05-02 10:48:44 +0530

    Somthinng is added

--- src/pages/coupons/components/Screen1.jsx
+++ src/pages/coupons/components/Screen1.jsx
@@ -1,8 +1,7 @@
-import React, { useState } from 'react'
 
 function Screen1() {
   return (
-    <div className="screen1 h-[600px] px-[10%] flex justify-center  flex-col gap-10 lg:border-b relative">
+    <div className="screen1 h-[600px] px-[10%] hidden lg:flex justify-center  flex-col gap-10 lg:border-b relative">
       <h1 data-aos="fade-up" className="text-4xl lg:text-6xl font-bold z-10">Get Your Coupons</h1>
       <div className="flex justify-center items-end">
         <button className="py-[15px] pr-[45px] pl-[35px] border border-[#F43F5E]  text-2xl clip text-[#F43F5E] cursor-pointer z-10">Connect With MetaMask</button>
--- src/pages/coupons/components/Screen2.jsx
+++ src/pages/coupons/components/Screen2.jsx
@@ -25,12 +25,12 @@ function Screen2() {
 
 
   return (
-    <div className="screen2   p-[10%] space-y-10">
+    <div className="screen2 my-20 md:mt-0  p-[10%] space-y-10">
       <h1 data-aos="fade-up" className="text-4xl lg:text-6xl font-bold z-10">Your Coupons</h1>
       <table className="w-full border rounded-md">
         <thead className='h-12'>
           <tr>
-            <th>SL.NO</th>
+            <th className='hidden md:inline-block'>SL.NO</th>
             <th>Coupon Code</th>
             <th>Value</th>
             <th>Redeem</th>
@@ -41,7 +41,7 @@ function Screen2() {
             
         </tbody>
       </table>
-      <button className="py-[15px] pr-[45px] pl-[35px] border border-[#F43F5E]  text-2xl clip text-[#F43F5E] cursor-pointer z-10 float-right">Connect With MetaMask</button>
+      <button className="py-[15px] pr-[45px] pl-[35px] border border-[#F43F5E]  text-lg md:text-2xl clip text-[#F43F5E] cursor-pointer z-10 float-right">Connect With MetaMask</button>
     </div>
   )
 }
@@ -50,12 +50,12 @@ export default Screen2
 
 const TableRow = ({code,value,index}) => {
   return (
-    <tr className='text-center border h-20 hover:scale-105 duration-500'>
-      <td>{index+1}</td>
+    <tr className='text-center border h-20 hover:scale-105 duration-500 text-sm md:text-base'>
+      <td className='hidden md:inline-block'>{index+1}</td>
       <td>{code}</td>
       <td>{value} SAT</td>
       <td>
-        <button className='py-[10px] pr-[45px] pl-[35px] border border-[#F43F5E]  text-2xl clip text-[#F43F5E] cursor-pointer z-10'>Redeem</button>
+        <button className='py-[10px] pr-[20px] md:pr-[45px] pl-[15px] md:pl-[35px] border border-[#F43F5E]  md:text-2xl clip text-[#F43F5E] cursor-pointer z-10'>Redeem</button>
       </td>
     </tr>
   )
